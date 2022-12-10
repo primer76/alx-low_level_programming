dd6ca0b9003a5f424521667e0df93f0f0abfecfd
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-09 18:55:00 -0800

    Task 2 done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdion.h>
+
+/**
+ * main - lists alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char = alphabets;
+	alphabets = 'a';
+
+	while (alphabets <= 'z')
+	{
+		putchar("%c ", alphabets);
+		alphabets++;
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 C - Variables, if, else, while
 0. Assigns random number to the variable each time it is executed
 1. Assigns random number to the variabe and prints the last digit
+2. Prints alphabets in lowercase
