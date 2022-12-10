bda28febbfbe0a9282ac0e99278d1f9a448ebbf1
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-09 20:59:22 -0800

    Task 8 done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - all numbers of bas 16 in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int f;
+	char j;
+
+	for (f = 48; f < 58; f++)
+	{
+		putchar(f);
+	}
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
