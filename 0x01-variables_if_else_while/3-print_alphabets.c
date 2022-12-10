515ab9c528b7162310ebb24202f30d1c167e2894
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-09 19:45:13 -0800

    Task 3 done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	char y;
 
-	for (y = 'a'; y <= 'z', y++)
+	for (y = 'a'; y <= 'z'; y++)
 		putchar(y);
-	for (y = 'A'; y <= 'Z', y++)
+	for (y = 'A'; y <= 'Z'; y++)
 		putchar(y);
 	putchar('\n');
 	return (0);
