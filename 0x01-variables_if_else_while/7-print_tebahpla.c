d0dc3384990682678c45637be4d7dbcf874c96b1
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-09 20:50:51 -0800

    TASK 7 DONE

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - writes letters in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char j;
+
+	for (j = 'z'; j >= 'a'; z--)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
