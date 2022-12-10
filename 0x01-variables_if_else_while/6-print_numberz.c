5aff3f1cc85dc28ab2d8e54ed66f4c78cdb10cce
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-09 20:40:28 -0800

    Task 6 done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 48; l < 58; l++)
+	{
+		putchar(l);
+	}
+	putchar("\n");
+	return (0);
+}
