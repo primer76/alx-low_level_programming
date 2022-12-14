735c692e7a7cd9585a6566c008056c7f99f30aa2
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-14 14:56:19 -0800

    Task 0 done

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * positive_or_negative - contains code to check positive or negative
+ * @i : code to be checked
+ * Return: Always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{
+		printf("%dis zero\n", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
