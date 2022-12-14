720e2f846948eebad45180ee90f740dcd6e4df71
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-13 19:42:37 -0800

    Task 14 done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 fibonacci numbers, starting from 1 and 2
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	long int b, c, d, forward;
+
+	c = 1;
+	d = 2;
+
+	for (b = 1; b <= 50; ++b)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		}
+		else
+		{
+			printf("%ld\n", b);
+		}
+		forward = c + d;
+		c = d;
+		d = forward;
+	}
+	return (0);
+}
