1be44a5d66d4cfdca4170bf3ca87487b7a484a69
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-13 19:11:01 -0800

    Task 12 done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((z / 10) + '0');
 					_putchar((z % 10) + '0');
-				} else if (z > 100)
+				} else if (z >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
