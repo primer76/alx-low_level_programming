4363028f8af09ccf8d8e1ecdb181b17b8c880014
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-15 10:06:31 -0800

    task 1 done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase letter
+ * @c : code to be checked
+ * Return: 1 if uppercase, 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'z');
+}
