b8f808acd14e42da9787a6c08df0529a6625e8e8
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-15 11:00:04 -0800

    Task 3 done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prits numbers 0 - 9
+ */
+
+void print_numbers(void)
+{
+	int k;
+
+	for (k = '0'; k <= 9; k++)
+	{
+		_putchar(k + '0')
+	}
+	_putchar('\n');
+}
