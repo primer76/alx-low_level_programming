d90e357e5ae02561cb7aed5a3c981f54c279b88b
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-15 16:00:45 -0800

    Task 5 done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 10times
+ */
+
+void more_numbers(void)
+{
+int a, b;
+for (a = 0; a <= 10; a++)
+{
+for (b = 0; b < 15; b++)
+{
+if (b >= 10)
+{
+_putchar(b / 10 + '0');
+_putcahr(b % 10 + '0');
+}
+_putchar('\n');
+}
+}
+}
