d8252916651a1c49bd9d70b509d697591da3b4a1
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-15 16:27:57 -0800

    Task done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line  - draws straight line
+ */
+
+void print_line(void)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int w;
+for (w = 1; w <= n; w++)
+{
+_putchar('_')
+}
+_putchar('\n');
+}
+}
