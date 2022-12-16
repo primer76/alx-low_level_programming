85314d33ab1d8913ffe04421d5c433e3d85aa716
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-15 16:53:00 -0800

    Task done

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,17 +7,18 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int w;
-		for (w = 1; w <= n; w++)
-		{
-			_putchar('_')
-		}
-		_putchar('\n');
-	}
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int w;
+
+for (w = 1; w <= n; w++)
+{
+_putchar('_')
+}
+_putchar('\n');
+}
 }
