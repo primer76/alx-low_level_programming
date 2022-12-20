6cd53dc2dbb40c59025c28ddd0ad37c545b4d4a5
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-20 15:02:42 -0800

    Task done

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ *
+ * @s : parameter
+ * Return: integer converted from string
+ */
+
+int _atoi(char *s)
+{
+	unsigned int ass = 0;
+	int sig = 1;
+
+	do {
+		if (*s == '-')
+			sig *= -1;
+		else if (*s >= '0' && *s <= '9')
+			ass = (ass * 10) + (*s - '0');
+		else if (ass > 0)
+			break;
+	} while (*s++);
+
+	return (ass * sif);
+}
