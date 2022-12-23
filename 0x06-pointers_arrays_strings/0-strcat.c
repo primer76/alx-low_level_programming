bc3a55066d33f6e0cea4f2b83c418a3d5fee0b91
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-23 15:12:31 -0800

    Task 1 done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,17 +12,15 @@ char *_strcat(char *dest, char *src)
 	int a, b;
 
 	a = 0;
-	while (dest[a] != '\0')
+	while (dest[a])
 	{
-		++a;
+		a++;
 	}
 
-	for (b = 0; src[b] != '\0'; ++b, ++a)
+	for (b = 0; src[b]; b++)
 	{
 		dest[a] = src[b];
 	}
-
-	dest[a] = '\0'
 
 	return (dest);
 }
