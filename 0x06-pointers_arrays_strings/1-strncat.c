5d38a8551ed65794cc85bd55f882060824dd828b
Anderson Ovie Ogedegbe <andyofficial5440@gmail.com>
2022-12-23 15:39:20 -0800

    task done

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates 2 strings together
+ * @dest : string 1
+ * @src : string 2
+ * @n : most number of bytes
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int w, x;
+
+	w = 0;
+
+	while (dest[w])
+		w++;
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
+		dest[w + x] = src[x];
+
+	dest[w + x] = '\0';
+	_putchar('\n');
+
+	return (dest);
+}
